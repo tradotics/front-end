57ae33a7dd0c6655c74fd4f0263cbd2ea6075fcd
Arun-mec <arunalakkalpalla@gmail.com>
2022-10-04 19:30:44 +0530

    journel page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 // import Doughnutchart from './components/Journel/Doughnutchart';
 import { Sidebar } from './contexts/Sidebarcontext';
-import Journelpage from './pages/Journelpage/Journelpage';
+// import Journelpage from './pages/Journelpage/Journelpage';
 import Landingpage from './pages/Landingpage/Landingpage';
-import Tradespage from './pages/Tradespage/Tradespage';
+// import Tradespage from './pages/Tradespage/Tradespage';
 
 function App() {
   return (
